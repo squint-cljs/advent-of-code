713452a9c3db6acef7cdbd0d6096f6b3e0257546
Michiel Borkent <michielborkent@gmail.com>
2022-09-02 12:58:49 +0200

    initial commit

---
+++ 2021/aoc01_borkdude.cljs
@@ -0,0 +1,18 @@
+(ns aoc01-borkdude
+  (:require ["fs" :as fs]))
+
+(def input
+  (mapv js/parseInt (.split (.trim (str (fs/readFileSync "2021/input/aoc01.txt"))) "\n")))
+
+(defn increases [nums]
+  (filter true?
+          (map #(< %1 %2)
+               nums
+               (rest nums))))
+
+(def answer-01 (time (prn (count (increases input)))))
+
+(def sums
+  (map #(+ % %2 %3) input (rest input) (rest (rest input))))
+
+(def answer-02 (time (prn (count (increases sums)))))
---
+++ README.md
@@ -0,0 +1,15 @@
+# Advent of Code in ClavaScript
+
+The goal of this repo is to exercise ClavaScript on Advent of Code puzzles to
+find bugs and missing features. Please submit your solution and anything you
+find in ClavaScript that needs fixing as an issue.
+
+Run `npm install` to install ClavaScript.
+
+Then run:
+
+```
+$ node node_modules/.bin/clava run 2021/aoc01_borkdude.cljs
+```
+
+to compile and run a solution.
